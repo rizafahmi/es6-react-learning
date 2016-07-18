b30f945d65a37cfdb2031f04fc1f8a826e19c00f
Riza Fahmi <rizafahmi@gmail.com>
2016-07-18 15:58:49 +0700

    Playing with props {4}

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
   render() {
-    return <h1>Hello React</h1>
+    let txt = this.props.text
+    return (
+      <div>
+        <h1>{txt}</h1>
+        <h2>{this.props.cat}</h2>
+      </div>
+    )
   }
 }
+
+App.propTypes = {
+  text: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  text: "This is default text"
+}
+
+ReactDOM.render(
+  <App cat="50" />,
+  document.getElementById('app')
+)
+
+// Stateless React
+// const App = () => {
+//   return <h1>Hello eggheads</h1>
+// }
+
 
 export default App
